639376be6beda85b50a1a2e7415635e3dc61248c
Nicole Bieber <nbieber@alum.mit.edu>
2014-02-27 08:43:43 -0500

    configure postgres db

--- app.rb
+++ app.rb
@@ -8,7 +8,23 @@ require './models'
 # enable :sessions
 # use Rack::Flash, :sweep => true
 
-set :database, "sqlite3:///glitter.sqlite3"
+configure :development do
+	set :database, "sqlite3:///glitter.sqlite3"
+end
+
+configure :production do
+ db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///localhost/mydb')
+
+ ActiveRecord::Base.establish_connection(
+   :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+   :host     => db.host,
+   :username => db.user,
+   :password => db.password,
+   :database => db.path[1..-1],
+   :encoding => 'utf8'
+ )
+end
+
 set :sessions, true
 
 get '/' do
